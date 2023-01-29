245fa621c500a7a0580e2d4ab2c4b080e0010f6b
Bishawjit-Mondol <bishawjitmondol29@gmail.com>
2023-01-29 19:14:06 +0600

    UI on screen view ok

--- _private/footer.php
+++ _private/footer.php
@@ -2,22 +2,23 @@
 					<div class="wrap">
 						<div class="footer-grid">
 							<h3>Our Units</h3>
-							<?u_list('units');?>
+							<?php u_list('units'); ?>
 						</div>
 						<div class="footer-grid">
 							<h3>Our Management</h3>
-							<?u_list('management');?>
+							<?php u_list('management'); ?>
 						</div>
 						<div class="footer-grid">
 							<h3>Career</h3>
-							<?u_list('career');?>
+							<?php u_list('career'); ?>
 						</div>
 						<div class="footer-grid">
 							<h3>Contact</h3>
-							<?u_list('contact');?>
+							<?php u_list('contact'); ?>
 							<p class="copy-right" style='text-align:right;'>
 								Copyright &#169; <a href="http://<?php echo $domain; ?>" target="_blank"><?php echo $companyname; ?></a>.
-								<br/>Developed by <a href="http://www.uniquewebers.com/" target="_blank">Unique Webers Ltd.</a></p>
+								<br />Developed by <a href="http://www.uniquewebers.com/" target="_blank">Unique Webers Ltd.</a>
+							</p>
 						</div>
 						<div class="clear"> </div>
 					</div>
--- index.php
+++ index.php
@@ -1,86 +1,99 @@
 <?php
-		date_default_timezone_set('Asia/Dhaka');
-	    include("_private/db.php");
-		dbconnect();
-		$companyname=get_fixedtext(1);
-		$home_heading=get_fixedtext(2);
-		$home_intro=get_fixedtext(3);
-		$domain=get_fixedtext(4);
-		$admin_email=get_fixedtext(5);
-		$slogan=get_fixedtext(6);
-		$page=$_GET['page'];
+date_default_timezone_set('Asia/Dhaka');
+include("_private/db.php");
+dbconnect();
+$companyname = get_fixedtext(1);
+$home_heading = get_fixedtext(2);
+$home_intro = get_fixedtext(3);
+$domain = get_fixedtext(4);
+$admin_email = get_fixedtext(5);
+$slogan = get_fixedtext(6);
+if (!empty($_GET['page'])) {
+	$page = $_GET['page'];
+} else {
+	$page = 'home';
+}
+//$page = $_GET['page'];
 ?>
 <!DOCTYPE HTML>
 <html>
-	<head>
-		<link rel="shortcut icon" type="image/x-icon" href="favicon.ico"/>
-		<title><?=$companyname?> - <?=$slogan?></title>
-		<link href="css/style.css" rel='stylesheet' type='text/css' />
-		<meta name="viewport" content="width=device-width, initial-scale=1">
-		<script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script>
-		</script>
-	    <script type="text/javascript" src="js/jquery.min.js"></script>
-	    <link rel="stylesheet" type="text/css" href="css/slider-style.css" />
-		<script type="text/javascript" src="js/modernizr.custom.28468.js"></script>
 
-		<script src="js/login.js"></script>
-		<link type="text/css" rel="stylesheet" href="css/jquery.mmenu.all.css" />
-		<script type="text/javascript" src="js/jquery.mmenu.js"></script>
-			<script type="text/javascript">
-				//	The menu on the left
-				$(function() {
-					$('nav#menu-left').mmenu();
-				});
-		</script>
-	</head>
-	<body>
-			<div id="page">
-					<div id="header">
-						<a class="navicon" href="#menu-left"> </a>
-					</div>
-					<nav id="menu-left">
-						<ul>
-							<?include("_private/menu.php");?>
-							<div class="clear"> </div>
-						</ul>
-					</nav>
-			</div>
-			<div class="header">
-				<div class="wrap">
-				<div class="header-left">
-					<div class="logo">
-						<a href="index.php?page=home"><img src="images/logo.png"><?=$companyname?></a>
-					</div>
+<head>
+	<link rel="shortcut icon" type="image/x-icon" href="favicon.ico" />
+	<title><?= $companyname ?> - <?= $slogan ?></title>
+	<link href="css/style.css" rel='stylesheet' type='text/css' />
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<script type="application/x-javascript">
+		addEventListener("load", function() {
+			setTimeout(hideURLbar, 0);
+		}, false);
+
+		function hideURLbar() {
+			window.scrollTo(0, 1);
+		}
+	</script>
+	</script>
+	<script type="text/javascript" src="js/jquery.min.js"></script>
+	<link rel="stylesheet" type="text/css" href="css/slider-style.css" />
+	<script type="text/javascript" src="js/modernizr.custom.28468.js"></script>
+
+	<script src="js/login.js"></script>
+	<link type="text/css" rel="stylesheet" href="css/jquery.mmenu.all.css" />
+	<script type="text/javascript" src="js/jquery.mmenu.js"></script>
+	<script type="text/javascript">
+		//	The menu on the left
+		$(function() {
+			$('nav#menu-left').mmenu();
+		});
+	</script>
+</head>
+
+<body>
+	<div id="page">
+		<div id="header">
+			<a class="navicon" href="#menu-left"> </a>
+		</div>
+		<nav id="menu-left">
+			<ul>
+				<?php include("_private/menu.php"); ?>
+				<div class="clear"> </div>
+			</ul>
+		</nav>
+	</div>
+	<div class="header">
+		<div class="wrap">
+			<div class="header-left">
+				<div class="logo">
+					<a href="index.php?page=home"><img src="images/logo.png"><?= $companyname ?></a>
 				</div>
-				<div class="header-right">
-					<div class="top-nav">
+			</div>
+			<div class="header-right">
+				<div class="top-nav">
 					<ul>
-						<?include("_private/menu.php");?>
+						<?php include("_private/menu.php"); ?>
 					</ul>
 				</div>
 				<div class="clear"> </div>
-				</div>
-				<div class="clear"> </div>
 			</div>
-			</div>
-			<?php if(($page=='home')||(!$page)){slides();}?>
-			<div class="content">
-				<?php
-					if($page)
-					{
-						$path="_private/content/".$page.".php";
-						if(file_exists("$path"))
-						{
-							include("$path");
-						}
-						else
-						{
-							include("_private/content/default.php");
-						}
-					}
-					include("_private/footer.php");
-				?>
-			</div>
-	</body>
-</html>
+			<div class="clear"> </div>
+		</div>
+	</div>
+	<?php if (($page == 'home') || (!$page)) {
+		slides();
+	} ?>
+	<div class="content">
+		<?php
+		if ($page) {
+			$path = "_private/content/" . $page . ".php";
+			if (file_exists("$path")) {
+				include("$path");
+			} else {
+				include("_private/content/default.php");
+			}
+		}
+		include("_private/footer.php");
+		?>
+	</div>
+</body>
 
+</html>
