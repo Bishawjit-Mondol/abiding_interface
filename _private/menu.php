ae26da39ac9bf85ba9fa20c9910f2c80eb02d337
Bishawjit-Mondol <bishawjitmondol29@gmail.com>
2023-01-31 13:16:34 +0600

    menu fix

--- _private/menu.php
+++ _private/menu.php
@@ -6,21 +6,19 @@
   <a class="dropbtn">About</a>
   <div class="dropdown-content">
     <a href="#">Corporate Profile</a>
-	<a href="index.php?page=about">Company Profile</a>
+    <a href="index.php?page=about">Company Profile</a>
     <a href="#">Mission, Vision & Values</a>
   </div>
 </div>
 
 <!-- Career -->
-<li><a href="index.php?page=career">Career</a></li>
-
-<!-- Contact -->
+<!-- <li><a href="index.php?page=career">Career</a></li> -->
 <div class="dropdown">
-  <a class="dropbtn">Contact</a>
+  <a class="dropbtn">Career</a>
   <div class="dropdown-content">
-    <a href="#">Corporate Office</a>
-    <a href="#">Sales Office</a>
-    <a href="#">Factory</a>
+    <a href="#">Marketing & Sales</a>
+    <a href="#">Engineer</a>
+    <a href="#">Procurement</a>
   </div>
 </div>
 
@@ -39,6 +37,16 @@
     <a href="#">ADHL</a>
     <a href="#">ABSL</a>
     <a href="#">ATIL</a>
-	<a href="#">UW</a>
+    <a href="#">UW</a>
+  </div>
+</div>
+
+<!-- Contact -->
+<div class="dropdown">
+  <a class="dropbtn">Contact</a>
+  <div class="dropdown-content">
+    <a href="#">Corporate Office</a>
+    <a href="#">Sales Office</a>
+    <a href="#">Factory</a>
   </div>
 </div>
