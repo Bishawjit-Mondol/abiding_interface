7a9c2da6f36ec8bd49a9207973155f710abb3829
Bishawjit-Mondol <bishawjitmondol29@gmail.com>
2023-02-10 16:26:12 +0600

    adhl ourteam menu fix

--- adhl/_private/content.php
+++ adhl/_private/content.php
@@ -1,21 +1,13 @@
 <?php
-	if($page)
-	{
-		$path="_private/inc/content/".$page.".php";
-		if(($page_pagetype_id<1)&&(file_exists("$path")))
-		{
-			include("$path");
-		}
-		elseif($page_id&&$page_pagetype)
-		{
-			include("_private/inc/content/".$page_pagetype.".php");
-		}
-		else
-		{
-			include("_private/inc/content/default.php");
-		}
+if ($page) {
+	$path = "_private/inc/content/" . $page . ".php";
+	if (($page_pagetype_id < 1) && (file_exists("$path"))) {
+		include("$path");
+	} elseif ($page_id && $page_pagetype) {
+		include("_private/inc/content/" . $page_pagetype . ".php");
+	} else {
+		include("_private/inc/content/default.php");
 	}
-	else
-    {
-		include("_private/inc/content/home.php");
-    }
+} else {
+	include("_private/inc/content/home.php");
+}
